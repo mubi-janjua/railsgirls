ff8a7a5d186be85930502c1c804c01d9511d67c0
mubi-janjua <mubeenaftabjanjua@gmail.com>
2020-05-29 20:44:35 +0500

    second commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_28_195249) do
+ActiveRecord::Schema.define(version: 2020_05_29_111957) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "user_name"
+    t.text "body"
+    t.integer "idea_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "ideas", force: :cascade do |t|
     t.string "name"
